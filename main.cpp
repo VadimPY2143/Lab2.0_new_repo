14466b36e0c978aec633d3ef4c28de76b468accf
Vadim Papusha <137208219+VadimPY2143@users.noreply.github.com>
2025-09-18 16:23:43 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,9 +18,9 @@ int main()
                 + 2 / (a - sqrt(2 * a)))
                 * (sqrt(a) - sqrt(2)) / (a + 2);
 
-    // double z2 = 1 / (sqrt(a) + sqrt(2));
+    double z2 = 1 / (sqrt(a) + sqrt(2));
     cout << endl;
     cout << "z1 = " << z1 << endl;
-    // cout << "z2 = " << z2 << endl;
+    cout << "z2 = " << z2 << endl;
     return 0;
 }
